880744c96ddf1d276d01c1defb5947cc3df71709
User <martinezleandro044@gmail.com>
2023-12-10 16:22:19 +1300

    changes ip

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,5 +12,5 @@ func main() {
 	router.POST("/generate", d.CollectorHandler.GenerateSubtitles)
 	router.GET("/videos/:word", d.CollectorHandler.GetVideosByWord)
 	router.GET("/check", d.CollectorHandler.Check)
-	router.Run()
+	router.Run("0.0.0.0:8080")
 }
