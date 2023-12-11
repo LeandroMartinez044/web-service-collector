794f314960a55617a3a03d9c4aed3cd165c559af
User <martinezleandro044@gmail.com>
2023-12-11 18:14:35 +1300

    dsadas

--- internal/repositories/youtube/rest.go
+++ internal/repositories/youtube/rest.go
@@ -30,7 +30,7 @@ func (repo *repository) GenerateSubtitlesFile(videoId string) error {
 
 	//filename := buildDirectoryPath() + "/" + "%(title)s.%(ext)s"
 
-	cmd := exec.Command("/usr/local/bin/youtube-dl", "--write-sub", "--sub-lang", "en", "--skip-download", "-o", outputDirectory+"/"+videoURL+".%(ext)s", videoURL)
+	cmd := exec.Command("/bin/bash", "youtube-dl --write-sub", "--sub-lang", "en", "--skip-download", "-o", outputDirectory+"/"+videoURL+".%(ext)s", videoURL)
 
 	// Run the command
 	_, err := cmd.CombinedOutput()
